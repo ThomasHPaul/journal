1d3a2be2ca705f36043d5457827b2eb05ca15eef
Thomas Paul <thomaspauljs@outlook.com>
2023-05-25 06:47:03 -0500

    Finished Dao interface implementation

--- src/main/java/com/journal/repository/AbstractDao.java
+++ src/main/java/com/journal/repository/AbstractDao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class AbstractDao {
+public abstract class AbstractDao {
 
     protected Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/journal_db";
--- src/main/java/com/journal/repository/EntryDao.java
+++ src/main/java/com/journal/repository/EntryDao.java
@@ -2,10 +2,9 @@ package com.journal.repository;
 
 import com.journal.model.Entry;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class EntryDao extends AbstractDao implements Dao<Entry> {
@@ -29,6 +28,7 @@ public class EntryDao extends AbstractDao implements Dao<Entry> {
                 if(rset.next()) {
                     resEntry.setId(rset.getLong("id"));
                     resEntry.setWhenCreated(rset.getDate("whenCreated"));
+                    resEntry.setEntryText(rset.getString("entryText"));
                 }
 
                 entry = Optional.of(resEntry);
@@ -39,5 +39,99 @@ public class EntryDao extends AbstractDao implements Dao<Entry> {
         }
 
         return entry;
+    }
+
+    @Override
+    public List<Entry> findAll() {
+        String sql = "select * from entry";
+        List<Entry> entries = new ArrayList<>();
+
+        try (
+                Connection con = getConnection();
+                PreparedStatement prepStmt = con.prepareStatement(sql);
+                ) {
+            try (
+                    ResultSet rset = prepStmt.executeQuery();
+                    ) {
+                while(rset.next()) {
+                    Entry resEntry = new Entry();
+                    resEntry.setId(rset.getLong("id"));
+                    resEntry.setWhenCreated(rset.getDate("whenCreated"));
+                    resEntry.setEntryText(rset.getString("entryText"));
+
+                    entries.add(resEntry);
+                }
+            }
+        }
+        catch(SQLException sqe) {
+            sqe.printStackTrace();
+        }
+
+        return entries;
+    }
+
+    @Override
+    public Entry update(Entry entry) {
+        String sql = "update entry set entryText = ? where id = ?";
+
+        try (
+                Connection con = getConnection();
+                PreparedStatement prepStmt = con.prepareStatement(sql);
+                ) {
+            prepStmt.setString(1, entry.getEntryText());
+            prepStmt.setLong(2, entry.getId());
+            prepStmt.execute();
+        }
+        catch(SQLException sqe) {
+            sqe.printStackTrace();
+        }
+        return entry;
+    }
+
+    @Override
+    public Entry create(Entry entry) {
+        String sql = "insert into entry (whenCreated, entryText) values (?, ?)";
+
+        try (
+                Connection con = getConnection();
+                PreparedStatement prepStmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ) {
+            prepStmt.setDate(1, entry.getWhenCreated());
+            prepStmt.setString(2, entry.getEntryText());
+
+            prepStmt.executeUpdate();
+
+            try (
+                    ResultSet genKeys = prepStmt.getGeneratedKeys();
+                    ) {
+                if(genKeys.next()) {
+                    entry.setId(genKeys.getLong(1));
+                }
+            }
+        }
+        catch(SQLException sqe) {
+            sqe.printStackTrace();
+        }
+    return entry;
+    }
+
+    @Override
+    public int delete(Entry entry) {
+        String sql = "delete from entry where id = ?";
+        int rowsAffected = 0;
+
+        try (
+                Connection con = getConnection();
+                PreparedStatement prepStmt = con.prepareStatement(sql)
+                ) {
+            prepStmt.setLong(1, entry.getId());
+
+            rowsAffected = prepStmt.executeUpdate();
+        }
+        catch(SQLException sqe) {
+            sqe.printStackTrace();
+        }
+
+        return rowsAffected;
     }
 }
